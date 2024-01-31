3df2ab569e975113f7a121135e7712d20cba2938
Ashish Prabhu K <ashishprabhu2143@gmail.com>
2024-01-31 22:10:13 +0530

    updates

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,16 @@
 const http = require('http');
+require('dotenv').config();
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify('Howdy peeps!'));
+    res.end(JSON.stringify('ashishprabhuk'));
 });
 
-const port = 3000;
+// const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
+
+require('dotenv').config(); // Load environment variables from .env file
